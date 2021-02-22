fc184cc23565fbce834e08fe81af8db24caa6b3a
crotaphiticus <jpaul@jpaul.com.ar>
2021-02-22 14:48:46 -0300

    Initial Commit

---
+++ script.py
@@ -0,0 +1,43 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+# print(sys.executable)
+
+
+# def greet (who_to_greet) :
+#     greeting = 'hello, {}'.format(who_to_greet)
+#     return greeting
+
+# print (greet('Juan'))
+
+
+# def saludo (a_quien_saludar):
+#     saludos = 'hola {}'.format(a_quien_saludar)
+#     return saludo
+
+
+# print (saludo('Juan'))
+
+
+# def saludar (pasar_variable_de_a_quien_saludar):
+#     saludos = 'Hola {}'.format(pasar_variable_de_a_quien_saludar)
+#     return saludos
+
+# print (saludar('Juan'))
+
+
+# def hola(valores_que_toma):
+#     test1 = "Estoy tomando este valor {}".format(valores_que_toma)
+#     return test1
+
+
+# # print (hola ( '...SOY ESE VALOR') )
+
+# r = requests.get("https://infobae.com")
+# print(r.status_code)
+
+name = input("Your name ?")
+print("Hello, ", (name))
